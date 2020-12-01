ccdbb5c8dad3515da803b4162857f408d1e573ec
Justin Stephenson <jstephen@redhat.com>
2020-12-01 11:28:59 -0500

    KCM: Read and set KCM krb5 options
    These options are necessary to call into existing
    krb5 child code(handle_child_send).
    
    Allow some options to be provided in [kcm] configuration section
    
    krb5_renew_interval
    krb5_renew_lifetime
    krb5_lifetime
    krb5_validate
    krb5_canonicalize

--- src/responder/kcm/kcm.c
+++ src/responder/kcm/kcm.c
@@ -27,8 +27,11 @@
 #include "responder/kcm/kcmsrv_ccache.h"
 #include "responder/kcm/kcmsrv_pvt.h"
 #include "responder/common/responder.h"
+#include "providers/krb5/krb5_common.h"
 #include "util/util.h"
 #include "util/sss_krb5.h"
+
+extern struct dp_option default_krb5_opts[];
 
 #define DEFAULT_KCM_FD_LIMIT 2048
 #define DEFAULT_KCM_CLI_IDLE_TIMEOUT 300
@@ -83,10 +86,100 @@ static errno_t kcm_get_ccdb_be(struct kcm_ctx *kctx)
     return EOK;
 }
 
-static int kcm_get_config(struct kcm_ctx *kctx)
+static int kcm_get_krb5_config(struct kcm_ctx *kctx,
+                               struct krb5_ctx *krb5_ctx)
+{
+    errno_t ret;
+    char *rtime = NULL;
+    char *lifetime_str = NULL;
+    bool validate = false;
+
+    /* Lifetime */
+    ret = confdb_get_string(kctx->rctx->cdb,
+                            kctx->rctx,
+                            kctx->rctx->confdb_service_path,
+                            CONFDB_KCM_KRB5_LIFETIME,
+                            0, &lifetime_str);
+    if (ret != 0) {
+        DEBUG(SSSDBG_OP_FAILURE, "Cannot get confdb lifetime\n");
+        goto done;
+    }
+
+    if (lifetime_str != 0) {
+        ret = krb5_string_to_deltat(lifetime_str, &krb5_ctx->lifetime);
+        if (ret != 0) {
+            DEBUG(SSSDBG_OP_FAILURE, "Failed to convert lifetime string.\n");
+            goto done;
+        }
+        krb5_ctx->lifetime_str = lifetime_str;
+    }
+
+    /* Renewable lifetime */
+    ret = confdb_get_string(kctx->rctx->cdb,
+                            kctx->rctx,
+                            kctx->rctx->confdb_service_path,
+                            CONFDB_KCM_KRB5_RENEWABLE_LIFETIME,
+                            0, &rtime);
+    if (ret != 0) {
+        DEBUG(SSSDBG_OP_FAILURE, "Cannot get confdb renewable lifetime\n");
+        goto done;
+    }
+
+    if (rtime != 0) {
+        ret = krb5_string_to_deltat(rtime, &krb5_ctx->rlife);
+        if (ret != 0) {
+            DEBUG(SSSDBG_OP_FAILURE, "Failed to convert renewable lifetime string.\n");
+            goto done;
+        }
+        krb5_ctx->rlife_str = rtime;
+    }
+
+    /* Validate */
+    ret = confdb_get_bool(kctx->rctx->cdb,
+                          kctx->rctx->confdb_service_path,
+                          CONFDB_KCM_KRB5_VALIDATE,
+                          false, &validate);
+    if (ret != 0) {
+        DEBUG(SSSDBG_OP_FAILURE, "Cannot get confdb krb5 validate\n");
+        goto done;
+    }
+
+    ret = dp_opt_set_bool(krb5_ctx->opts, KRB5_VALIDATE, validate);
+    if (ret != 0) {
+        DEBUG(SSSDBG_OP_FAILURE, "Cannot set dp opt krb5 validate\n");
+        goto done;
+    }
+
+    /* Canonicalize */
+    ret = confdb_get_bool(kctx->rctx->cdb,
+                          kctx->rctx->confdb_service_path,
+                          CONFDB_KCM_KRB5_CANONICALIZE,
+                          false, &krb5_ctx->canonicalize);
+    if (ret != 0) {
+        DEBUG(SSSDBG_OP_FAILURE, "Cannot get confdb krb5 canonicalize\n");
+        goto done;
+    }
+
+    return EOK;
+done:
+    return ret;
+}
+
+static int kcm_get_config(struct kcm_ctx *kctx,
+                          struct krb5_ctx **_krb5_ctx)
 {
     int ret;
     char *sock_name;
+    struct krb5_ctx *krb5_ctx;
+    int i;
+
+    krb5_ctx = talloc_zero(kctx->rctx, struct krb5_ctx);
+    if (krb5_ctx == NULL) {
+        DEBUG(SSSDBG_FATAL_FAILURE,
+              "fatal error allocating krb5_ctx\n");
+        ret = ENOMEM;
+        goto done;
+    }
 
     ret = confdb_get_int(kctx->rctx->cdb,
                          CONFDB_KCM_CONF_ENTRY,
@@ -96,6 +189,7 @@ static int kcm_get_config(struct kcm_ctx *kctx)
     if (ret != EOK) {
         DEBUG(SSSDBG_FATAL_FAILURE,
               "Failed to get file descriptors limit\n");
+        talloc_free(krb5_ctx);
         goto done;
     }
 
@@ -108,6 +202,7 @@ static int kcm_get_config(struct kcm_ctx *kctx)
         DEBUG(SSSDBG_OP_FAILURE,
               "Cannot get the client idle timeout [%d]: %s\n",
                ret, strerror(ret));
+        talloc_free(krb5_ctx);
         goto done;
     }
 
@@ -126,6 +221,7 @@ static int kcm_get_config(struct kcm_ctx *kctx)
         DEBUG(SSSDBG_OP_FAILURE,
               "Cannot get KCM socket path [%d]: %s\n",
                ret, strerror(ret));
+        talloc_free(krb5_ctx);
         goto done;
     }
     kctx->rctx->sock_name = sock_name;
@@ -135,6 +231,7 @@ static int kcm_get_config(struct kcm_ctx *kctx)
         DEBUG(SSSDBG_OP_FAILURE,
               "Cannot get KCM ccache DB [%d]: %s\n",
                ret, strerror(ret));
+        talloc_free(krb5_ctx);
         goto done;
     }
 
@@ -152,8 +249,47 @@ static int kcm_get_config(struct kcm_ctx *kctx)
         DEBUG(SSSDBG_OP_FAILURE,
               "Cannot create KCM request queue [%d]: %s\n",
                ret, strerror(ret));
+        talloc_free(krb5_ctx);
         goto done;
     }
+
+    /* Set default Kerberos options, needed for renewals */
+    krb5_ctx->opts = talloc_zero_array(krb5_ctx, struct dp_option, KRB5_OPTS);
+    if (!krb5_ctx->opts) goto done;
+    for (i = 0; i < KRB5_OPTS; i++) {
+        krb5_ctx->opts[i].opt_name = default_krb5_opts[i].opt_name;
+        krb5_ctx->opts[i].type = default_krb5_opts[i].type;
+        krb5_ctx->opts[i].def_val = default_krb5_opts[i].def_val;
+        switch (krb5_ctx->opts[i].type) {
+            case DP_OPT_STRING:
+                ret = dp_opt_set_string(krb5_ctx->opts, i,
+                                        default_krb5_opts[i].def_val.string);
+                break;
+            case DP_OPT_BLOB:
+                ret = dp_opt_set_blob(krb5_ctx->opts, i,
+                                      default_krb5_opts[i].def_val.blob);
+                break;
+            case DP_OPT_NUMBER:
+                ret = dp_opt_set_int(krb5_ctx->opts, i,
+                                     default_krb5_opts[i].def_val.number);
+                break;
+            case DP_OPT_BOOL:
+                ret = dp_opt_set_bool(krb5_ctx->opts, i,
+                                      default_krb5_opts[i].def_val.boolean);
+                break;
+        }
+        if (ret != 0) {
+            DEBUG(SSSDBG_OP_FAILURE, "Failed setting default KCM kerberos options\n");
+            talloc_free(krb5_ctx->opts);
+            talloc_free(krb5_ctx);
+            goto done;
+        }
+    }
+
+    /* Override with config options */
+    kcm_get_krb5_config(kctx, krb5_ctx);
+
+    *_krb5_ctx = krb5_ctx;
     ret = EOK;
 done:
     return ret;
@@ -210,6 +346,7 @@ static int kcm_process_init(TALLOC_CTX *mem_ctx,
 {
     struct resp_ctx *rctx;
     struct kcm_ctx *kctx;
+    struct krb5_ctx *krb5_ctx;
     int ret;
 
     rctx = talloc_zero(mem_ctx, struct resp_ctx);
@@ -236,7 +373,7 @@ static int kcm_process_init(TALLOC_CTX *mem_ctx,
     kctx->rctx = rctx;
     kctx->rctx->pvt_ctx = kctx;
 
-    ret = kcm_get_config(kctx);
+    ret = kcm_get_config(kctx, &krb5_ctx);
     if (ret != EOK) {
         DEBUG(SSSDBG_FATAL_FAILURE, "fatal error getting KCM config\n");
         goto fail;
